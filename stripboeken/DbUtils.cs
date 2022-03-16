ee2504ed5549d0ef05e70244145d2b6fabfcd4dd
Stan <Wiekstras@gmail.com>
2022-03-16 19:04:23 +0100

    Update mainpage and added folders for model & repos

---
+++ stripboeken/DbUtils.cs
@@ -0,0 +1,20 @@
+using System.Data;
+using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+
+namespace stripboeken
+{
+    public class DbUtils : IDbUtils
+    {
+        public IDbConnection GetDbConnection()
+        {
+            string connectionString = "Server=localhost;Port=8889;Database=nhl_stenden_cafe;Uid=root;Pwd=root;";
+            return new MySqlConnection(connectionString);
+        }
+    }
+
+    public interface IDbUtils
+    {
+        IDbConnection GetDbConnection();
+    }
+}
