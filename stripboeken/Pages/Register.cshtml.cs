b3ae7f36504956e20b9f1d586502a442230c5219
Jeenth01 <jeen.te.hennepe@student.nhlstenden.com>
2022-03-17 12:24:58 +0100

    Login & Register Added

---
+++ stripboeken/Pages/Register.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace stripboeken.Pages
+{
+    public class Register : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
